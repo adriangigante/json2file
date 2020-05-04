83934b1145669495f8265e8418d1f2e39e4957a7
adriangigante <agigante2012@gmail.com>
2020-05-04 18:02:03 +1000

    Fix vars

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ proxyUrl = "http://localhost:8080"
 #Docker
 #proxyUrl = "http://host.docker.internal:8080"
 
-webroot = "/Users/agigante/git/surfcam/html/"
-
+#webroot = "/Users/agigante/git/surfcam/html/"
+webroot = "/var/www/html/"
--- cron.py
+++ cron.py
@@ -1,9 +1,24 @@
 import config
 import schedule
 import time
-import json2file
-import parse_report
+#import json2file
+#import parse_report
 import datetime
+import replace_html
+import parse_report_inplace
+
+# def job():
+
+# 	print("cron-log: Running job at ")
+# 	print (datetime.datetime.now().time())
+# 	report = {"https://www.swellnet.com/reports/australia/new-south-wales/eastern-beaches", "http://www.swellnet.com/reports/australia/new-south-wales/northern-beaches", "https://www.coastalwatch.com/surf-cams-surf-reports/nsw/bondi-beach", "https://www.coastalwatch.com/surf-cams-surf-reports/nsw/manly---nth-steyne"}
+    
+# 	for url in report:
+# 	    print("cron-log: Creating report " + url)
+# 	    filename = json2file.run(config.proxyUrl + "/" + url)
+# 	    #print(filename)
+# 	    report = parse_report.run(filename)
+# 	    print("cron-log: Report created at " + report)   
 
 def job():
 
@@ -12,11 +27,14 @@ def job():
 	report = {"https://www.swellnet.com/reports/australia/new-south-wales/eastern-beaches", "http://www.swellnet.com/reports/australia/new-south-wales/northern-beaches", "https://www.coastalwatch.com/surf-cams-surf-reports/nsw/bondi-beach", "https://www.coastalwatch.com/surf-cams-surf-reports/nsw/manly---nth-steyne"}
     
 	for url in report:
-	    print("cron-log: Creating report " + url)
-	    filename = json2file.run(config.proxyUrl + "/" + url)
-	    #print(filename)
-	    report = parse_report.run(filename)
-	    print("cron-log: Report created at " + report)   
+		print("cron-log: Creating report " + url)
+		contents = urllib2.urlopen(config.proxyUrl + "/" + url).read()
+		# print(contents)
+		data = json.loads(contents)
+		html = data["contents"].encode("utf-8")
+		fullReport = parse_report_inplace.run(url, html)
+		# print(fullReport)
+		replace_html.run(url,fullReport)
 
 #Run on start
 job()
